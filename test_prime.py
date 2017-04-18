96469f12357b30256667edd10ff8b0ed90f57348
Donna Mwiine <dmwiine@gmail.com>
2017-04-18 12:39:50 +0300

    Made changes to the tests

--- test_prime.py
+++ test_prime.py
@@ -1,26 +1,25 @@
 import unittest
-
+from prime import Prime
 class TestPrime(unittest.TestCase):
-	
-	def test_prime_returns_right_prime_numbers(self):
-		result = prime(10)
-		self.assertEqual([2,3,5,7], result, msg="Incorrect output")
+	def setUp(self):
+		self.prime = Prime()
 
 	def test_prime_returns_error_if_arg_is_not_positive_integer(self):
-		self.assertRaises(TypeError, self.prime, -10)
+		self.assertRaises(TypeError, self.prime.prime, -10)
 
 	def test_prime_returns_error_message_if_arg_is_string(self):
-		self.assertRaises(TypeError, self.prime, '10')
+		self.assertRaises(TypeError, self.prime.prime, '10')
 
 	def test_prime_returns_error_message_if_arg_is_float(self):
-		self.assertRaises(TypeError, self.prime, 10.0)
+		self.assertRaises(TypeError, self.prime.prime, 10.0)
 
-	def test_prime_returns_empty_list_when_arg_is_0():
-		self.assertEqual([], result, msg="Incorrect result")
+	def test_prime_returns_empty_list_when_arg_is_0(self):
+		result = self.prime.prime(0)
+		self.assertEqual([], result)
 
-	def test_prime_returns_true_when_arg_is_2():
-		result = prime(2)
-		self.assertEqual([2], result, msg="Incorrect result")
+	def test_prime_returns_2_when_arg_is_2(self):
+		result = self.prime.prime(2)
+		self.assertEqual(2, result)
 
 
 	if __name__ == '--main--':
