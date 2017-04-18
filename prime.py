86b0ce1c10692181b82d3460b46c60e5e8b08de0
Donna Mwiine <dmwiine@gmail.com>
2017-04-18 12:43:19 +0300

    Made changes to the prime function

--- prime.py
+++ prime.py
@@ -1,20 +1,24 @@
-def prime(n):
-	prime_numbers = []
-	if isinstance(n,int) and n > 0:
-		if n==0:
-			print([])
-		else:
-			for num in range(0, n+1):
-				if num > 1:
-					for i in range(2,num):
-						if num%i == 0:
-							break
-							'''numd = numbers.remove(num)'''
-					else:
-						print(num)
-	else:
-		raise TypeError('Argument must be positive integer')
 
-prime(10)
+class Prime(object):
+	def prime(self,n):
+		prime_numbers = []
+		if isinstance(n,int) and n > 0:
+			if n==0:
+				print([])
+			elif n == 2:
+				print([2])
+			else:
+				for num in range(0, n+1):
+					if num > 1:
+						for i in range(2,num):
+							if num%i == 0:
+								break
+							'''numd = numbers.remove(num)'''
+						else:
+							print(num)
+		else:
+			raise TypeError('Argument must be positive integer')
+
+'''prime(10)
 prime(-7)
-prime('10')
+prime('10')'''
